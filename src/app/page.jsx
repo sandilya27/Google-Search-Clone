cc11d97492e0280481c4626103bcab5be0ae73c6
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-03-02 02:44:40 +0530

    added body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
 import HomeHeader from "@/components/HomeHeader"
+import HomeSearch from "@/components/HomeSearch"
+import Image from "next/image"
 
 
 const Home = () => {
   return (
     <>
       <HomeHeader/>
+      <div className="flex flex-col items-center mt-24 gap-2">
+        <Image 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzxDNWh7hH3GUnryIpBGSdLFdcZigSz2TDXvp5x-ybhw&s"
+          alt="logo"
+          width={300}
+          height={100}
+          priority
+        />
+        <HomeSearch/>
+      </div>
     </>
   )
 }
